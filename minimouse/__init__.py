fd97a7770b6591f3bb05f5b8d6b266c49a9f6ac6
Luís Garrote <80787932+luisgarrote@users.noreply.github.com>
2026-02-17 11:10:17 +0000

    Add CanvasRenderer to module exports

--- minimouse/__init__.py
+++ minimouse/__init__.py
@@ -5,6 +5,7 @@ This library provides a simulation environment for micromouse maze solving.
 """
 
 from .env import MicromouseEnv
+from .canvas import CanvasRenderer
 
 __version__ = "0.1.0"
-__all__ = ["MicromouseEnv"]
+__all__ = ["MicromouseEnv","CanvasRenderer"]
